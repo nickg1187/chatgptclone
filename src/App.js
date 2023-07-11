75312e4830a00188e6e0904ff4393910fb4458af
Nick Grundberg <nick.grundberg@gmail.com>
2023-07-11 13:19:54 -0400

    connected api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const getMessages = async () => {
+    const options = {
+      method: "POST",
+      body: JSON.stringify({
+        message: "hello how are you",
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    try {
+      const response = await fetch(
+        "http://localhost:8000/completions",
+        options
+      );
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section className="side-bar">
+        <button>+ New Chat</button>
+        <ul className="history">
+          <li>weeeee</li>
+        </ul>
+        <nav>
+          <p>Created by Nick Grundberg</p>
+        </nav>
+      </section>
+      <section className="main">
+        <h1>NickGPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit" onClick={getMessages}>
+              ➢
+            </div>
+          </div>
+          <p className="info">somethin about chat gpt</p>
+        </div>
+      </section>
     </div>
   );
 }
